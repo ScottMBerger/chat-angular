28bbff76dae2be3ae6380065227dddfcfe5942a0
Scott Berger <contact@scottberger.xyz>
2016-04-19 15:15:59 +0000

    Add animations

--- client/main.js
+++ client/main.js
@@ -1,4 +1,5 @@
 import angular from 'angular';
+import ngAnimate from 'angular-animate';
 import angularMeteor from 'angular-meteor';
 import hashtags from './hashtags';
 import messages from './messages';
@@ -6,7 +7,7 @@ import messages from './messages';
 
 var app = angular.module('simple-todos', [
   angularMeteor,
+  ngAnimate,
   hashtags.name,
-  messages.name,
-  
+  messages.name
 ]);
